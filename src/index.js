ae8708852f57c215fc713a0f99ddf66e9fbada89
yukio yamamoto <aj.yamamoto@me.com>
2016-04-22 23:32:34 -1000

    using papaparse for csv

--- src/index.js
+++ src/index.js
@@ -1,3 +1,12 @@
-import ready from 'domready'
+/* eslint console: 0*/
+// import ready from 'domready';
 
-ready(() =>document.querySelector('body').innerHTML = '<h1>Hello, World</h1>')
+// ready(() => document.querySelector('body').innerHTML = '<h1>Hello, World</h1>');
+import papaparse from 'papaparse';
+
+const completed = (results, file) => {
+  console.log('file', file);
+  console.log('results', results);
+};
+
+papaparse.parse('../data/coral_bleaching.csv', { complete: completed });
